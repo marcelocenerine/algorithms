458987b4346dcdc14f5ab5aa227ff0a34da7e06b
Marcelo Cenerino <marcelocenerine@gmail.com>
2015-10-26 18:07:57 +0000

    implement quick sort in scala

--- src/main/scala/com/cenerino/algorithms/scala/sort/QuickSort.scala
+++ src/main/scala/com/cenerino/algorithms/scala/sort/QuickSort.scala
@@ -1,18 +1,18 @@
 package com.cenerino.algorithms.scala.sort
 
 /**
- *
+ * Created by marcelo on 26/10/15.
  */
 object QuickSort {
 
-  def sort(xs: Array[Int]): Array[Int] = {
-    if (xs.length <= 1) xs
+  def sort(array: Array[Int]): Array[Int] = {
+    if (array.length <= 1) array
     else {
-      val pivot = xs(xs.length / 2)
+      val pivot = array(array.length / 2)
       Array.concat(
-        sort(xs filter (pivot >)),
-        xs filter (pivot ==),
-        sort(xs filter (pivot <)))
+        sort(array filter (pivot >)),
+        array filter (pivot ==),
+        sort(array filter (pivot <)))
     }
   }
 }
