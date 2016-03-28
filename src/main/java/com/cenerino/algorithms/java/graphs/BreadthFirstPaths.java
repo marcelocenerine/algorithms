83a7fb4526a96352d61ae6654899f0594c6c33d6
Marcelo Cenerino <marcelocenerine@gmail.com>
2016-03-28 12:26:09 +0100

    implement breadth first paths

---
+++ src/main/java/com/cenerino/algorithms/java/graphs/BreadthFirstPaths.java
@@ -0,0 +1,56 @@
+package com.cenerino.algorithms.java.graphs;
+
+import java.util.*;
+
+import static java.util.Collections.emptyList;
+
+public class BreadthFirstPaths<V> {
+
+    private Set<V> visited = new HashSet<>();
+    private Map<V, V> edgeTo = new HashMap<>();
+    private V source;
+
+    private BreadthFirstPaths(Graph<V> graph, V source) {
+        this.source = source;
+        bfs(graph, source);
+    }
+
+    public static <V> BreadthFirstPaths<V> from(Graph<V> graph, V source) {
+        return new BreadthFirstPaths<>(graph, source);
+    }
+
+    private void bfs(Graph<V> graph, V source) {
+        Queue<V> queue = new LinkedList<>();
+        queue.add(source);
+        visited.add(source);
+
+        while (!queue.isEmpty()) {
+            V vertex = queue.remove();
+
+            for (V adj : graph.adj(vertex)) {
+                if (!visited.contains(adj)) {
+                    queue.add(adj);
+                    visited.add(adj);
+                    edgeTo.put(adj, vertex);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(V target) {
+        return visited.contains(target);
+    }
+
+    public Iterable<V> pathTo(V target) {
+        if (!hasPathTo(target)) return emptyList();
+
+        Deque<V> path = new LinkedList<>();
+
+        for (V v = target; v != source; v = edgeTo.get(v)) {
+            path.push(v);
+        }
+
+        path.push(source);
+        return path;
+    }
+}
---
+++ src/test/java/com/cenerino/algorithms/java/graphs/BreadthFirstPathsTest.java
@@ -0,0 +1,58 @@
+package com.cenerino.algorithms.java.graphs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class BreadthFirstPathsTest {
+
+    private BreadthFirstPaths<Integer> dfp;
+
+    @Before
+    public void setUp() {
+        Graph<Integer> graph = new UndirectedGraph<>();
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 6);
+        graph.addEdge(6, 4);
+        graph.addEdge(4, 3);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 3);
+        graph.addEdge(5, 0);
+
+        dfp = BreadthFirstPaths.from(graph, 0);
+    }
+
+    @Test
+    public void shouldHavePath() {
+        assertThat(dfp.hasPathTo(5), is(true));
+    }
+
+    @Test
+    public void shouldReturnPath() {
+        assertThat(dfp.pathTo(5), is(asList(0, 5)));
+    }
+
+    @Test
+    public void shouldNotHavePath() {
+        assertThat(dfp.hasPathTo(7), is(false));
+    }
+
+    @Test
+    public void shouldReturnEmptyPath() {
+        assertThat(dfp.pathTo(7), is(emptyList()));
+    }
+}
